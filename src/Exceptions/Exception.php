fea7808a594247acacfada4dcb3788c34c5b65eb
cangbug66 <836166092@qq.com>
2019-08-29 21:50:20 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/25
+ * Time: 13:06
+ */
+namespace CreatedBug\Weather\Exceptions;
+
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/25
+ * Time: 13:09
+ */
+namespace CreatedBug\Weather\Exceptions;
+
+use CreatedBug\Weather\Exceptions\Exception;
+
+class HttpException extends Exception
+{
+
+}
